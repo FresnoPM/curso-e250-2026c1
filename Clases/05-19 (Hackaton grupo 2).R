8fda3f7e72ee679313f42df4483e5c63d51249a4
Lucía Fresno <Lucafresno@gmail.com>
2026-05-19 12:12:37 -0300

    Inicio de Hackaton impromptu

--- Clases/05-19 (Hackaton grupo 2).R
+++ Clases/05-19 (Hackaton grupo 2).R
@@ -2,3 +2,22 @@ library(igraph)
 library(igraphdata)
 data()
 g <-
+
+# Característica de la red
+#     ¿Cuántos nodos y aristas tiene? summary(g)
+# ¿Qué atributos tienen los nodos y las aristas? Es dirigida o no dirigida? Es ponderada o no ponderada?
+#     ¿La red está conectada? Si no, ¿cuántos componentes tiene y qué tamaño tiene el más grande?
+#     Cómo se  conecta
+# Grado promedio y la mediana; Son parecidos o muy distintos? Qué dice eso sobre la red?
+#     Histograma de grados. Tiene cola larga?
+#     Cuáles son  los 5 nodos más conectados (hubs?)  Tiene sentido que sean hubs?
+#     Densidad? Es una red densa o esparsa?
+#     Estructura de la red
+# Coeficiente de clustering global. Es alto o bajo?
+#     Cuál es la distancia promedio entre nodos?
+#     Cuántas comunidades encontró
+# # Detección de comunidades (convertir a no dirigida si hace falta):
+# com <- cluster_louvain(as.undirected(g))
+# length(com)
+# sizes(com)
+
